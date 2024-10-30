745b3802c2751e9cd10a0b1de7cacbdbf81398d4
Riccardo Panico <rikkardo.panico@gmail.com>
2024-10-30 11:41:57 +0100

    fix

--- app/threads/websocket.py
+++ app/threads/websocket.py
@@ -3,49 +3,40 @@ import websockets
 import json
 import subprocess
 
-# Configurazione
-# HOST = 'localhost'  # IP locale della macchina
-HOST = '0.0.0.0'  # IP pubblico
-PORT = 8765  # Porta di comunicazione WebSocket
-
-# Set per tenere traccia dei client connessi
+HOST = '0.0.0.0'  
+PORT = 8765  
 connected_clients = set()
 
-# Funzione per gestire le connessioni WebSocket
 async def socket_handler(websocket, path):
-    # Aggiungi il client alla lista dei client connessi
+    
     connected_clients.add(websocket)
     try:
         async for message in websocket:
             print(f"Ricevuto: {message}")
             try:
-                # Tenta di decodificare il messaggio come JSON
                 try:
                     data = json.loads(message)
                 except json.JSONDecodeError:
                     print("Messaggio non in formato JSON, ignorato.")
                     continue
-                
-                # Verifica se il messaggio ha la chiave 'action'
                 if "action" in data:
                     action = data["action"]
-                    # Gestione delle azioni specifiche
                     if action == "ping":
-                        await websocket.send(json.dumps({"response": "pong"}))
-                    elif action == "spegni":
-                        print("Esecuzione comando spegni...")
+                        await websocket.send(json.dumps({"message": "pong"}))
+                    elif action == "poweroff":
+                        print("Esecuzione comando poweroff...")
                         subprocess.run(["clear"], shell=True)
                         subprocess.run(["sudo", "systemctl", "stop", "getty@tty1.service"], shell=False)
                         subprocess.run(["sudo", "poweroff", "--no-wall"], shell=False)
-                    elif action == "riavvia":
-                        print("Esecuzione comando riavvia...")
+                    elif action == "poweroff":
+                        print("Esecuzione comando poweroff...")
                         subprocess.run(["clear"], shell=True)
                         subprocess_result = subprocess.run(["sudo", "systemctl", "stop", "getty@tty1.service"], shell=False)
                         if subprocess_result.returncode == 0:
                             subprocess_result = subprocess.run(["sudo", "reboot", "--no-wall"], shell=False)
-                        print(f"Subprocess terminato con codice di ritorno: {subprocess_result.returncode}")
                     else:
                         print(f"Azione non riconosciuta: {action}")
+                    await websocket.send(json.dumps({"action": "readyForNext"}))
                 else:
                     print("Messaggio JSON non valido, manca la chiave 'action'.")
             except Exception as e:
@@ -53,24 +44,19 @@ async def socket_handler(websocket, path):
     except websockets.exceptions.ConnectionClosed as e:
         print(f"Connessione chiusa: {e}")
     finally:
-        # Rimuovi il client dalla lista quando la connessione si chiude
         connected_clients.remove(websocket)
         print("Client disconnesso")
 
-# Funzione per avviare il server WebSocket
 async def start_websocket_server():
     async with websockets.serve(socket_handler, HOST, PORT):
         print(f"Server WebSocket avviato su ws://{HOST}:{PORT}")
-        await asyncio.Future()  # Mantieni il server attivo
+        await asyncio.Future()  
 
-# Funzione per inviare un messaggio a tutti i client connessi
 async def broadcast_message(message):
     if connected_clients:
         await asyncio.wait([client.send(message) for client in connected_clients])
 
-# Funzione per avviare il server WebSocket come thread, utilizzando il contesto dell'app Flask
 def run(app):
-    # Utilizza il contesto dell'app Flask per accedere ai servizi dell'applicazione
     with app.app_context():
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)

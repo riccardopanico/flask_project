a42f29abbe393ccb5f9878d5693392792091bf21
Riccardo Panico <rikkardo.panico@gmail.com>
2024-10-30 11:43:01 +0100

    fix

--- app/jobs/my_job.py
+++ app/jobs/my_job.py
@@ -1,5 +1,5 @@
-import time
+# import time
 
-def run():
-    print(f"Job di esempio eseguito alle: {time.strftime('%Y-%m-%d %H:%M:%S')}")
-    # Logica del job da eseguire periodicamente
+# def run():
+#     print(f"Job di esempio eseguito alle: {time.strftime('%Y-%m-%d %H:%M:%S')}")
+#     # Logica del job da eseguire periodicamente

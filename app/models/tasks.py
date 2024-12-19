bd55d2474c7db9c58f81b62981b096ae2a869293
Riccardo Panico <riccardo.panico@niva.it>
2024-12-19 15:40:32 +0100

    task

--- app/models/tasks.py
+++ app/models/tasks.py
@@ -6,7 +6,9 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     device_id = db.Column(db.Integer, nullable=False)
     task_type = db.Column(db.String(50), nullable=False)
-    sent = db.Column(db.Integer, nullable=True, server_default='0')
+    sent_to_cloud = db.Column(db.Integer, nullable=True, server_default='0')
+    sent_to_data_center = db.Column(db.Integer, nullable=True, server_default='0')
+    sent_to_device = db.Column(db.Integer, nullable=True, server_default='0')
     status = db.Column(db.String(50), nullable=True)
     created_at = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
 
@@ -16,6 +18,8 @@ class Task(db.Model):
             "device_id": self.device_id,
             "task_type": self.task_type,
             "created_at": self.created_at.isoformat() if self.created_at else None,
-            "sent": self.sent,
+            "sent_to_cloud": self.sent_to_cloud,
+            "sent_to_data_center": self.sent_to_data_center,
+            "sent_to_device": self.sent_to_device,
             "status": self.status
         }

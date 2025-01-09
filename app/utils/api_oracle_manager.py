5e38629411d315a5e3b4c6150ca25cc96e3c9fe7
Riccardo Panico <riccardo.panico@niva.it>
2025-01-09 14:46:30 +0100

    fix

--- app/utils/api_oracle_manager.py
+++ app/utils/api_oracle_manager.py
@@ -2,6 +2,7 @@ import os
 import requests
 import base64
 from requests.exceptions import RequestException
+from flask import current_app
 
 class ApiOracleManager:
     def __init__(self):
@@ -18,7 +19,7 @@ class ApiOracleManager:
     def _get_access_token(self):
         """Ottiene un nuovo access token."""
 
-        print("into _get_access_token")
+        current_app.logger.debug("into _get_access_token")
         credentials = f"{self.client_id}:{self.client_secret}"
         encoded_credentials = base64.b64encode(credentials.encode('utf-8')).decode('utf-8')
 
@@ -30,9 +31,7 @@ class ApiOracleManager:
 
         try:
             response = requests.post(self.api_token_url, headers=headers, data=payload)
-            print(f"Response: {response}")
-            print(f"Response.status_code: {response.status_code}")
-            print(f"Token response: {response.json().get('access_token')}")
+            current_app.logger.debug(f"Token response: {response.json().get('access_token')}")
             if response.status_code == 200:
                 data = response.json()
                 self.access_token = data['access_token']
@@ -41,6 +40,7 @@ class ApiOracleManager:
             else:
                 return {'success': False, 'error': response.text , 'status_code': response.status_code}
         except RequestException as e:
+            current_app.logger.error(f"Errore durante la richiesta del token: {e}")
             return {'success': False, 'error': str(e)}
 
     def call(self, url, params=None, method='GET'):
@@ -50,8 +50,7 @@ class ApiOracleManager:
             if not token_response['success']:
                 raise Exception(f"Errore nel recupero del token: {token_response['error']}")
         full_url = f"{self.api_base_url}/{url.lstrip('/')}"
-        full_url = url
-        print(f"Chiamata API: {full_url}")
+        current_app.logger.debug(f"Chiamata API: {full_url}")
         method = method.upper()
 
         try:
@@ -65,13 +64,15 @@ class ApiOracleManager:
                 response = requests.get(full_url, params=params, headers=self.headers)
 
             if response.status_code == 200 or response.status_code == 201:
-                return {'success': True, 'data': response.json()}
+                return response.json()
             elif response.status_code == 401:
                 # Token non valido, rigeneralo
                 self._get_access_token()
                 return self.call(url, params, method)
             else:
+                current_app.logger.error(f"Errore nella chiamata API: {response.text}")
                 return {'success': False, 'status': response.status_code, 'error': response.text}
 
         except RequestException as e:
+            current_app.logger.error(f"Errore durante la chiamata API: {e}")
             return {'success': False, 'status': 500, 'error': str(e)}

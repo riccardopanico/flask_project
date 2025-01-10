086610abb74309ea13a3f5abca5c6cb6cd3c8be3
Riccardo Panico <riccardo.panico@niva.it>
2025-01-10 11:22:04 +0100

    fix

--- app/jobs/sync_devices_from_cloud.py
+++ app/jobs/sync_devices_from_cloud.py
@@ -14,8 +14,7 @@ JOB_INTERVAL = timedelta(seconds=5)
 def run(app):
     with app.app_context():
         try:
-            if current_app.debug:
-                current_app.logger.debug("Sincronizzazione dei dispositivi in corso...")
+            current_app.logger.info("Inizio sincronizzazione dispositivi.")
             Session = sessionmaker(bind=db.engine)
             session = Session()
 
@@ -33,30 +32,14 @@ def run(app):
             for record in data_records:
                 record = {key.lower(): value for key, value in record.items()}
                 synchronized_device_ids.append(record['device_id'])
+
                 device = session.query(Device).filter_by(device_id=record['device_id']).first()
 
-                # Ottieni l'API manager associato al dispositivo
+                # Gestione di api_manager
                 api_manager = app.api_device_manager.get(record['username'])
-                if not api_manager:
-                    current_app.logger.info(f"Device manager non trovato per il dispositivo {record['username']}. Creazione in corso...")
-
-                    # Controlla che i parametri esistano e siano validi
-                    if not all([
-                        'ip_address' in record and record['ip_address'],
-                        'username' in record and record['username'],
-                        'password' in record and record['password']
-                    ]):
-                        current_app.logger.warning(f"Parametri mancanti o non validi per il dispositivo {record['username']}.")
-                    else:
-                        api_manager = ApiDeviceManager(
-                            ip_address=record['ip_address'],
-                            username=record['username'],
-                            password=record['password']
-                        )
-                        app.api_device_manager[record['username']] = api_manager
 
                 if not device:
-                    # Crea un nuovo dispositivo e utente associato
+                    current_app.logger.info(f"Dispositivo non trovato: {record['device_id']}. Creazione in corso...")
                     user = session.query(User).filter_by(username=record['username']).first()
                     if not user:
                         user = User(username=record['username'], user_type=record['user_type'])
@@ -65,13 +48,23 @@ def run(app):
                         user.last_name = record.get('last_name')
                         user.email = record.get('email')
                         session.add(user)
-                        session.flush()  # Ottiene l'ID del nuovo utente senza effettuare il commit
-                        current_app.logger.debug(f"Creato nuovo utente: {record['username']}")
+                        session.flush()
+                        current_app.logger.info(f"Utente creato: {record['username']}")
 
                     device = Device(user_id=user.id, device_id=record['device_id'])
                     session.add(device)
-                    current_app.logger.debug(f"Creato nuovo dispositivo associato all'utente: {record['username']} con ID dispositivo: {record['device_id']}")
+                    current_app.logger.info(f"Dispositivo creato: {record['device_id']}")
 
+                    # Crea il ApiDeviceManager al momento della creazione
+                    api_manager = ApiDeviceManager(
+                        ip_address=record['ip_address'],
+                        username=record['username'],
+                        password=record['password']
+                    )
+                    app.api_device_manager[record['username']] = api_manager
+                    current_app.logger.info(f"ApiDeviceManager creato per il dispositivo: {record['device_id']}")
+
+                    # Registra il dispositivo tramite api_manager
                     api_response = api_manager.call(
                         'auth/register',
                         params={
@@ -84,11 +77,8 @@ def run(app):
                         method='POST'
                     )
 
-                    if api_response.get('success'):
-                        if current_app.debug:
-                            current_app.logger.debug(f"Aggiornamento delle credenziali per il dispositivo {device.username} avvenuto con successo.")
-                    else:
-                        current_app.logger.warning(f"Aggiornamento delle credenziali per il dispositivo {device.username} fallito: {api_response.get('error')}")
+                    if not api_response.get('success'):
+                        current_app.logger.warning(f"Registrazione fallita per il dispositivo {record['device_id']}: {api_response.get('error')}")
 
                 else:
                     # Aggiorna i dati del dispositivo e dell'utente associato
@@ -97,73 +87,68 @@ def run(app):
                         if user.username != record['username']:
                             existing_user = session.query(User).filter_by(username=record['username']).first()
                             if existing_user and existing_user.id != user.id:
-                                if current_app.debug:
-                                    current_app.logger.warning(f"Errore: L'username '{record['username']}' è già in uso da un altro utente.")
+                                current_app.logger.warning(f"Errore: Username '{record['username']}' già in uso.")
                                 continue
                             user.username = record['username']
-                        user.user_type = record.get('user_type', user.user_type)
-                        if 'password' in record and record['password']:
-                            if user.password_hash is None or not user.check_password(record['password']):
-                                user.set_password(record['password'])
 
+                        user.user_type = record.get('user_type', user.user_type)
+                        if 'password' in record and record['password'] and not user.check_password(record['password']):
+                            user.set_password(record['password'])
+                            current_app.logger.info(f"Password aggiornata per l'utente: {user.username}")
+                            if not api_manager:
+                                current_app.logger.warning(f"ApiDeviceManager non trovato per {record['device_id']} ({record['username']}). Aggiornamento credenziali non possibile.")
+                            else:
                                 api_response = api_manager.call(
                                     'auth/update_credentials',
-                                    params={'new_password': record['password'], 'new_username': record['username']},
+                                    params={
+                                        'new_password': record['password'],
+                                        'new_username': record['username']
+                                    },
                                     method='POST'
                                 )
-                                if api_response.get('success'):
-                                    api_manager.username = record['username']
-                                    api_manager.password = record['password']
-                                    if current_app.debug:
-                                        current_app.logger.info(f"Password aggiornata correttamente per dispositivo: {record['device_id']}")
-                                else:
-                                    if current_app.debug:
-                                        current_app.logger.error(f"Errore durante l'aggiornamento della password per dispositivo {record['device_id']}: {api_response.get('error')}")
+                                if not api_response.get('success'):
+                                    current_app.logger.error(f"Errore aggiornamento credenziali per {record['device_id']}: {api_response.get('error')}")
 
                         user.name = record.get('name', user.name)
                         user.last_name = record.get('last_name', user.last_name)
                         user.email = record.get('email', user.email)
-                        if current_app.debug:
-                            current_app.logger.debug(f"Dati utente aggiornati: {record['username']}")
 
-                # Aggiorna i dati del dispositivo se necessario
-                device.mac_address = record.get('mac_address', device.mac_address)
-                device.ip_address = record.get('ip_address', device.ip_address)
-                device.gateway = record.get('gateway', device.gateway)
-                device.subnet_mask = record.get('subnet_mask', device.subnet_mask)
-                device.dns_address = record.get('dns_address', device.dns_address)
-                device.username = record.get('username', device.username)
-                device.password = record.get('password', device.password)
+                    # Aggiorna i dati del dispositivo
+                    device.mac_address = record.get('mac_address', device.mac_address)
+                    device.ip_address = record.get('ip_address', device.ip_address)
+                    device.gateway = record.get('gateway', device.gateway)
+                    device.subnet_mask = record.get('subnet_mask', device.subnet_mask)
+                    device.dns_address = record.get('dns_address', device.dns_address)
+                    device.username = record.get('username', device.username)
+                    device.password = record.get('password', device.password)
 
-                #
-                api_manager.ip_address = record['ip_address']
+                    api_manager.ip_address = device.ip_address
+                    current_app.logger.info(f"Dispositivo aggiornato: {device.device_id}")
 
-                current_app.logger.debug(f"Dati dispositivo aggiornati per ID dispositivo: {record['device_id']}")
-
-            # Rimuovi i dispositivi e utenti di tipo "device" non sincronizzati
+            # Rimuovi dispositivi non sincronizzati
             devices_to_remove = session.query(Device).join(User).filter(
                 Device.device_id.notin_(synchronized_device_ids),
                 User.user_type == 'device'
             ).all()
             for device in devices_to_remove:
-                current_app.logger.debug(f"Rimozione dispositivo non sincronizzato: {device.device_id}")
+                current_app.logger.info(f"Rimozione dispositivo non sincronizzato: {device.device_id}")
                 if device.username in app.api_device_manager:
                     del app.api_device_manager[device.username]
-                    current_app.logger.debug(f"Rimosso ApiDeviceManager per dispositivo: {device.device_id}")
+                    current_app.logger.info(f"ApiDeviceManager rimosso per dispositivo: {device.device_id}")
                 session.delete(device)
 
                 user = session.query(User).filter_by(id=device.user_id, user_type='device').first()
                 if user:
                     session.delete(user)
-                    current_app.logger.debug(f"Rimosso utente associato al dispositivo: {device.device_id}")
+                    current_app.logger.info(f"Utente rimosso per il dispositivo: {device.device_id}")
 
             session.commit()
-            current_app.logger.info("Sincronizzazione dei record completata con successo.")
+            current_app.logger.info("Sincronizzazione completata con successo.")
         except IntegrityError as e:
             session.rollback()
-            current_app.logger.error(f"Errore di integrità durante la sincronizzazione dei record: {str(e)}", exc_info=True)
+            current_app.logger.error(f"Errore di integrità durante la sincronizzazione: {str(e)}", exc_info=True)
         except (SQLAlchemyError, Exception) as e:
             session.rollback()
-            current_app.logger.error(f"Errore durante la sincronizzazione dei record: {str(e)}", exc_info=True)
+            current_app.logger.error(f"Errore durante la sincronizzazione: {str(e)}", exc_info=True)
         finally:
             session.close()
